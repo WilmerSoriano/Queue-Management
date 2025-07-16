93d6c5881c360db9006d430b6d757670bbfe3a28
WilmerSoriano <official_Wilmer.2020@outlook.com>
2025-07-16 00:27:17 -0500

    DIP switch last update

--- Client/DIP.py
+++ Client/DIP.py
@@ -1,7 +1,7 @@
 from machine import Pin
 
 # GPIO pins connected to DIP switches (bit 0 to bit 5)(GP0 to GP5 on Pico)
-switch_pins = [0, 1, 2, 3, 4]  # Can expand later to [0, 1, 2, 3, 4, 5]
+switch_pins = [0, 1, 2, 3, 4, 5]  # Can expand later to [0, 1, 2, 3, 4, 5]
 
 switches = [Pin(pin, Pin.IN, Pin.PULL_UP) for pin in switch_pins]
 
